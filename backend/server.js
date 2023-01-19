b441bd4943182cfd988c08b8e91c7c9b3fcc22e5
steffFrank <--replace-all>
2023-01-19 18:47:07 +0100

    feat: Connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.set("strictQuery", false);
+
+try {
+    mongoose.connect(process.env.MONGODB_CONNECTION);
+    console.log("connected to mongoose");
+} catch(error) {
+    console.error(error);
+}
+
 
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
