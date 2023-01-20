d6cd587ac84fa16caddc7b7d945768f534413df8
steffFrank <--replace-all>
2023-01-20 07:51:58 +0100

    feat: Log user and send token

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ const User = require("./models/user");
 require("dotenv").config();
 mongoose.set("strictQuery", false);
 const bcrypt = require("bcrypt");
-const user = require("./models/user");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 try {
     mongoose.connect(process.env.MONGODB_CONNECTION);
@@ -38,15 +38,48 @@ app.post("/api/auth/signup", async (req, res) => {
     });
 
     try {
-        const response = await user.save()
-        console.log(response);
+        await user.save()
         res.status(201).json({message: "User registered with success"});
     } catch(error) {
-        console.log(error);
+        console.error(error);
         res.status(400).json({error});
     }      
 })
 
+app.post("/api/auth/login", async(req, res) => {
+    const { email, password } = req.body;
+    
+    const checkUser = async (email) => {
+        try {
+            const user = await User.findOne({email:email});
+            return user;
+        }catch(error) {
+            console.log(error);
+        }
+    }
+    const user = await checkUser(email);
+    const comparePassword = async (password, hash) => {
+        try {
+            return await bcrypt.compare(password, hash);
+        }catch(error) {
+            console.log(error);
+            res.status(404).json(error);
+        }
+        return false;
+    }
+    if (user) {
+        const isMatch = await comparePassword(password, user.password);
+        if (isMatch) {
+            const randomToken = jwt.sign(
+                { userId: user._id },
+                "RANDOM_TOKEN_SECRET",
+                { expiresIn: "24h" }
+            )
+            res.status(200).json({userId: user._id, token: randomToken});
+        }
+    }
+    res.status(404).json("Bad request");
+})
 
 
 
